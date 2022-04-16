5f98efaf7d9a1eb42c4ea63c7ef6b4bce68ff6b9
SpaceCat1748 <87800609+SpaceCat1748@users.noreply.github.com>
2022-04-16 16:12:02 +0300

    Add files via upload

---
+++ Star Hub.lua
@@ -0,0 +1,11 @@
+if game.PlaceId == 6284583030 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/BLACKGAMER1221/Pet-Simulator-X/main/BK%20Pet.lua"))()
+elseif game.PlaceId == 1537690962 then
+   loadstring(game:HttpGet("https://raw.githubusercontent.com/infinitylua/Luascripts/main/KJFsdEj8sXrja.lua"))()
+elseif game.PlaceId == 7305309231 then 
+   loadstring(game:HttpGet("https://raw.githubusercontent.com/x3fall3nangel/FallAngelHub/main/Taxi_Boss.lua"))()
+elseif game.PlaceId == 8554378337 then
+   loadstring(game:HttpGet("https://raw.githubusercontent.com/ZaRdoOx/weapon-figting/main/Weapon-Fighting-Simulator"))()
+elseif game.PlaceId == 8781849572 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/SpaceCat1748/Boblox/main/Star%20Sim%20beta.lua"))()
+end
