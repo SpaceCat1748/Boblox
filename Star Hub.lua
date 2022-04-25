5176d72b49862ded52acd1a21dd624994b4a22f2
SpaceCat1748 <87800609+SpaceCat1748@users.noreply.github.com>
2022-04-25 22:57:44 +0300

    Update Star Hub.lua

--- Star Hub.lua
+++ Star Hub.lua
@@ -1,13 +1,28 @@
 if game.PlaceId == 6284583030 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/MrKix/nullptrpsx/main/psxloadstring.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/MrKix/nullptrpsx/main/psxloadstring.lua"))() -- Pet Simulator X
 elseif game.PlaceId == 1537690962 then
-   loadstring(game:HttpGet("https://raw.githubusercontent.com/infinitylua/Luascripts/main/KJFsdEj8sXrja.lua"))()
+   loadstring(game:HttpGet("https://raw.githubusercontent.com/infinitylua/Luascripts/main/KJFsdEj8sXrja.lua"))() -- Bee Swarm Simulator
 elseif game.PlaceId == 7305309231 then 
-   loadstring(game:HttpGet("https://raw.githubusercontent.com/x3fall3nangel/FallAngelHub/main/Taxi_Boss.lua"))()
+   loadstring(game:HttpGet("https://raw.githubusercontent.com/x3fall3nangel/FallAngelHub/main/Taxi_Boss.lua"))() -- Taxi Boss
 elseif game.PlaceId == 8554378337 then
-   loadstring(game:HttpGet("https://raw.githubusercontent.com/ZaRdoOx/weapon-figting/main/Weapon-Fighting-Simulator"))()
+   loadstring(game:HttpGet("https://raw.githubusercontent.com/ZaRdoOx/weapon-figting/main/Weapon-Fighting-Simulator"))() -- Weapon Fighting Simulator
 elseif game.PlaceId == 8781849572 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/SpaceCat1748/Boblox/main/Star%20Sim%20beta.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/SpaceCat1748/Boblox/main/Star%20Sim%20beta.lua"))() -- Star Simulator Beta
 elseif game.PlaceId == 7708751418 then
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/SunkenMuch/Soggyware/main/Main',true))()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/SunkenMuch/Soggyware/main/Main',true))() -- Smashing Simulator X
+elseif game.PlaceId == 9049840490 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Babyhamsta/RBLX_Scripts/main/Sonic%20Speed%20Simulator/main.lua", true))() -- Sonic Speed Simulator
+elseif game.PlaceId == 2202352383 then
+    loadstring(game:HttpGet("https://www.toptal.com/developers/hastebin/raw/akugugako"))() -- Super Power Training Simulator
+elseif game.PlaceId == 6299805723 then 
+    loadstring(game.HttpGet(game, "https://raw.githubusercontent.com/KiJinGaming/FreeScript/main/KJHub.lua"))() -- Anime Fighters Simulator
+elseif game.PlaceId == 8982709021 then
+     game.StarterGui:SetCore("SendNotification", { -- SHOW NOTIFIACTION
+                Title = "Notification!", -- NOTIFICACTION LABEL
+                Text = "You need Krnl or Krnl DLL!", -- NOTIFICATION DESCRIPTION / TEXT
+                Icon = "", -- ICON (NO ICON)
+                Duration = 2.4 -- DURATION OF THE NOTIFIACTIOn
+            })
+wait(0.1)
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Scripts/main/YouTube%20Life/Auto%20Farm.lua"))() -- YouTube Life!
 end
